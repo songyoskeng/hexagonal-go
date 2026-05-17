63f87ba8801594284862623de439320b4fc9fe18
songyoskeng <songyoskeng@gmail.com>
2026-05-17 12:46:32 +0700

    step 3: update unit test use ForCalculateTax

--- taxcalculator/begin/tax_calculator_test.go
+++ taxcalculator/begin/tax_calculator_test.go
@@ -3,7 +3,7 @@ package begin
 import "testing"
 
 func TestCalculateTax(t *testing.T) {
-	taxCalculator := &TaxCalculator{}
+	taxCalculator := NewTaxCalculator() // refer ForCalculateTax
 	tax := taxCalculator.CalculateTax(10000)
 	if tax != 1000 {
 		t.Fatalf("expected 1000, got %v", tax)
