569e764df02873963770410766c3e6f1df46d186
songyoskeng <songyoskeng@gmail.com>
2026-05-17 12:57:50 +0700

    step 7: update unit test inject mock ForGetTaxRate

--- taxcalculator/begin/tax_calculator_test.go
+++ taxcalculator/begin/tax_calculator_test.go
@@ -3,7 +3,8 @@ package begin
 import "testing"
 
 func TestCalculateTax(t *testing.T) {
-	taxCalculator := NewTaxCalculator() // refer ForCalculateTax
+	taxRepository := NewTaxRateRepository()          // refer ForGetTaxRate
+	taxCalculator := NewTaxCalculator(taxRepository) // refer ForCalculateTax
 	tax := taxCalculator.CalculateTax(10000)
 	if tax != 1000 {
 		t.Fatalf("expected 1000, got %v", tax)
